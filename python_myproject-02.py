fdc5ab4db68ac5bea37a233410fa5e6790951863
Eren <81647285+ErenCelik96@users.noreply.github.com>
2021-04-24 13:53:57 +0300

    Update python_myproject-02.py

--- python_myproject-02.py
+++ python_myproject-02.py
@@ -1,29 +1,28 @@
-# project 2 Metro duraklar arası süre hesaplama
+# project 2 Metro duraklar arası süre hesaplama  # "durak adı yanlış girildiğinde hata verme sorunu"
 
 print('Lütfen sadece "kadıköy" veya "tavşantepe" seçeneklerinden birini giriniz..')
 kadıköy_yönü = {"Kadıköy" : 1, "Ayrılık Çeşmesi" : 2, "Acıbadem" : 3, "Ünalan" : 4, "Uzunçayır" : 5, "Göztepe" : 6, 
            "Yenisahra" : 7, "Kozyatağı" : 8, "Bostancı" : 9, "Küçükyalı" : 10, "Maltepe" : 11, "Huzurevi" :12,
            "Gülsuyu" : 13, "Esenkent" : 14, "Hastane Adliye" : 15, "Soğanlık" : 16, "Kartal" : 17, 
-           "Yakacık Adnan Kahveci" : 18, "Pendik" : 19, "Tavşantepe" : 20}
+           "Yakacık Adnan Kahveci" : 18, "Pendik" : 19, "Tavşantepe" : 20}  # kadıköy yönüne giderken durak sıraları
 tavşantepe_yönü = {"Kadıköy" : 20, "Ayrılık Çeşmesi" : 19, "Acıbadem" : 18, "Ünalan" : 17, "Uzunçayır" : 16, "Göztepe" : 15, 
            "Yenisahra" : 14, "Kozyatağı" : 13, "Bostancı" : 12, "Küçükyalı" : 11, "Maltepe" : 10, "Huzurevi" :9,
            "Gülsuyu" : 8, "Esenkent" : 7, "Hastane Adliye" : 6, "Soğanlık" : 5, "Kartal" : 4, 
-           "Yakacık Adnan Kahveci" : 3, "Pendik" : 2, "Tavşantepe" : 1}
-ort_süre = 2
+           "Yakacık Adnan Kahveci" : 3, "Pendik" : 2, "Tavşantepe" : 1}  # tavşantepe yönüne giderken durak sıraları
 
 while True:
-  yön = input("Hangi yöne gidiyorsunuz?").title()
+  yön = input("Hangi yöne gidiyorsunuz? ").title()
   mevcut = input("Hangi duraktasınız? ").title()
   hedef = input("Hangi durağa gideceksiniz? ").title()
-  if yön == "Kadıköy":
-    durak_sayısı = (int(kadıköy_yönü[mevcut] - kadıköy_yönü[hedef]))
-    sonuç = durak_sayısı * ort_süre
+  if yön == "Kadıköy":  # girilen yön kadıköy ise aşağıyı çalıştır
+    durak_sayısı = (int(kadıköy_yönü[mevcut] - kadıköy_yönü[hedef]))  # girilen yön durak numarasından hedefdeki durak numarası çıkarılır
+    sonuç = durak_sayısı * 2  # çıkan sayı 2 ile (duraklar arası ortalama süre 2 dk imiş) çarpılır
     print(f"Ortalama {sonuç} dakika sonra {hedef} durağına varacaksınız. İyi yolculuklar dileriz..")
     break
   elif yön == "Tavşantepe":
     durak_sayısı = (int(tavşantepe_yönü[mevcut] - tavşantepe_yönü[hedef]))
-    sonuç = durak_sayısı * ort_süre
+    sonuç = durak_sayısı * 2
     print(f"Ortalama {sonuç} dakika sonra {hedef} durağına varacaksınız. İyi yolculuklar dileriz..")
-    break
+    break  # tüm bilgiler doğru girilir ise sonucu verip programı durdur, değil ise uyarı verip başa sar
   else:
     print('Yanlış yön bilgisi girdiniz! Lütfen sadece "kadıköy" veya "tavşantepe" seçeneklerinden birini giriniz..')
